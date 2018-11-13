047c074e98e6588db4dc461479c17d6d9f9c6fe1
Aleksandr Dukhovskoy <alexdoohee@gmail.com>
2018-11-13 22:32:30 +0600

    improve

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/{pink-logo__*.svg,icon-editor-*.svg}")
+  return gulp.src("source/img/{pink-logo__*.svg,icon-editor-*.svg,logo-*.svg,icon-facebook.svg,icon-twitter.svg,icon-youtube.svg}")
     .pipe(svgstore({
       inlineSvg: true
     }))
